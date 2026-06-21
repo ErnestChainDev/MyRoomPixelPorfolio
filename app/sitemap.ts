9b5200cf88a747ff78eb9929daa235c4cb610433
Ernest Windel Dreo <ewdreo@sorsu.edu.ph>
2026-06-21 23:51:39 +0800

    EWD: Changing the site url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,12 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://hakandemir.com.tr";
+const siteUrl = "https://ernest-windel-dreo-portfolio.vercel.app";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: siteUrl,
-      lastModified: new Date("2026-06-13"),
+      lastModified: new Date("2026-06-21"),
       changeFrequency: "monthly",
       priority: 1,
     },
